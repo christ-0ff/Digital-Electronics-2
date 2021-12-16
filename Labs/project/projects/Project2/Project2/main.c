b56c5cfce1c4c1bdc9140011af77f34998fc0747
student <student@PC-625.urel.feec.vutbr.cz>
2021-12-16 12:23:27 +0100

    update project

--- Labs/project/projects/Project2/Project2/main.c
+++ Labs/project/projects/Project2/Project2/main.c
@@ -1,14 +1,14 @@
 /***********************************************************************
- * 
- * The I2C bus scanner detects the addresses of the modules that are 
- * connected to the SDA and SCL signals. A simple description of FSM is 
+ *
+ * The I2C bus scanner detects the addresses of the modules that are
+ * connected to the SDA and SCL signals. A simple description of FSM is
  * used.
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
  * Copyright (c) 2017-Present Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
- * 
+ *
  **********************************************************************/
 
 /* Defines -----------------------------------------------------------*/
@@ -23,13 +23,14 @@
 #include <stdlib.h>         // C library. Needed for conversion function
 #include "uart.h"           // Peter Fleury's UART library
 #include "twi.h"            // TWI library for AVR-GCC
+#include <util/delay.h>
 
 /* Functions ---------------------------------------------------------*/
     int32_t comp_temp(int32_t temp);
     uint32_t comp_press(int32_t press);
     uint32_t comp_hum(int32_t hum);
 /* Variables ---------------------------------------------------------*/
-	uint8_t result = 1;     // ACK result from the bus
+    uint8_t result = 1;     // ACK result from the bus
     char uart_string[33];   // String for converting numbers by itoa()
     
     
@@ -42,6 +43,10 @@
     int32_t c_temp;
     uint32_t c_press;
     uint32_t c_hum;
+    
+    float press1;
+    uint32_t press_frac = 256.00;
+    uint32_t hum_frac = 1024.00;    
     
     // Compensation parameters
     unsigned short dig_T1;
@@ -83,38 +88,40 @@ int main(void)
 
     // Configure 16-bit Timer/Counter1 to update FSM
     // Set prescaler to 33 ms and enable interrupt
-    //TIM1_overflow_33ms();
-    //TIM1_overflow_interrupt_enable();
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
     
-    //TIM0_overflow_4ms();
-    //TIM0_overflow_interrupt_enable();
+    TIM0_overflow_4ms();
+    TIM0_overflow_interrupt_enable();
     
     // Enables interrupts by setting the global interrupt mask
     sei();
     
-    /*
+    
     // Data compensation readout
     result = twi_start((0x76<<1) + TWI_WRITE);
-    		
+            
     //Result
     itoa(result, uart_string, 10);
-    uart_puts("data readout result:");
+    uart_puts("readout ");
     uart_puts(uart_string);
     uart_puts("\r\n");
     
     if (!result)
     {
         uint8_t comp_data[25];
+        
         twi_write(0x88);
         twi_start((0x76<<1) + TWI_READ);
-        for(int i = 0; i < 25; i++)
+        for(int i = 0; i <= 23; i++)
         {
             comp_data[i] = twi_read_ack();
-        }       
+        }
+            comp_data[24] = twi_read_nack();
         twi_stop();
         
     dig_T1 = ((unsigned short)comp_data[1] << 8) | ((unsigned short)comp_data[0]); //89,88
-    dig_T2 = ((short)comp_data[3] << 8) | ((short)comp_data[2]);                   //8B,8A 
+    dig_T2 = ((short)comp_data[3] << 8) | ((short)comp_data[2]);                   //8B,8A
     dig_T3 = ((short)comp_data[5] << 8) | ((short)comp_data[4]);                   //8C,8D
     
     dig_P1 = ((unsigned short)comp_data[7] << 8) | ((unsigned short)comp_data[6]); //8F,8E
@@ -128,56 +135,45 @@ int main(void)
     dig_P9 = ((short)comp_data[23] << 8) | ((short)comp_data[22]);                 //9F,9E
     
     dig_H1 = (unsigned char)comp_data[24];                                         //A1
-    }
-    */
-    
+    }  
+
+                        
+                 
     result = twi_start((0x76<<1) + TWI_WRITE);
     
     //Result
     itoa(result, uart_string, 10);
-    uart_puts("data readout2 result:");
+    uart_puts("readout2 ");
     uart_puts(uart_string);
     uart_puts("\r\n");
     
     if (!result)
-    {   
-        uint8_t comp_data[7];
-        twi_write(0xA1);
+    {
+        uint8_t comp_data1[7];
+        twi_write(0xE1);
         twi_start((0x76<<1) + TWI_READ);
-      
-            comp_data[0] = twi_read_ack();
-            //comp_data[1] = twi_read_ack();
-            //comp_data[2] = twi_read_ack();
-            //comp_data[3] = twi_read_ack();
-            //comp_data[4] = twi_read_ack();
-            //comp_data[5] = twi_read_ack();
-            //comp_data[6] = twi_read_nack();
-            
+        for(int i = 0; i <= 5; i++)
+        {
+            comp_data1[i] = twi_read_ack();
+        }
+            comp_data1[6] = twi_read_nack();
         twi_stop();
-        
-    //dig_H2 = ((short)comp_data[1] << 8) | ((short)comp_data[0]);                //E2,E1  data0 = E1, data1 = E2
-    //dig_H3 = (unsigned char)comp_data[2];                                       //E3     data2 = E3
-    //dig_H4 = (short)comp_data[4];                                               //E5,E4  data3 = E4, data4 = E5
-    //dig_H4 = ((short)comp_data[5] << 4);
-    //dig_H5 = ((short)comp_data[4] >> 4) | ((short)comp_data[5] << 4);           //E6,E5  data4 = E5, data5 = E6
-    //dig_H6 = comp_data[6];     //E7     data6 = E7
     
-    itoa(comp_data[0], uart_string, 16);
-    uart_puts("data2 ");
-    uart_puts(uart_string);
-    uart_puts("\r\n");       
+    dig_H2 = ((short)comp_data1[1] << 8) | ((short)comp_data1[0]);                //E2,E1  data0 = E1, data1 = E2
+    dig_H3 = (unsigned char)comp_data1[2];                                       //E3     data2 = E3
+    dig_H4 = (short)comp_data1[4];                                               //E5,E4  data3 = E4, data4 = E5
+    dig_H4 = ((short)comp_data1[3] << 4);
+    dig_H5 = ((short)comp_data1[4] >> 4) | ((short)comp_data1[5] << 4);           //E6,E5  data4 = E5, data5 = E6
+    dig_H6 = comp_data1[6];                                                       //E7     data6 = E7                 
     }
 
-
-                                                     
-    /*
     // Sensor INIT
     result = twi_start((0x76<<1) + TWI_WRITE);
     
     //Result
-    uart_puts("\f");
+    //uart_puts("\f");
     itoa(result, uart_string, 10);
-    uart_puts("init result:");
+    uart_puts("init result ");
     uart_puts(uart_string);
     uart_puts("\r\n");
     
@@ -190,112 +186,123 @@ int main(void)
         twi_write(0xF4);        //CTRL_MEAS
         twi_write(0b10110111);
         twi_stop();
-    }*/
+    }
 
     // Infinite loop
     while (1)
     {
-        // Empty loop. All subsequent operations are performed exclusively 
-        // inside interrupt service routines ISRs 
+        // Empty loop. All subsequent operations are performed exclusively
+        // inside interrupt service routines ISRs
     }
 
     // Will never reach this
     return 0;
 }
+
 /* Interrupt service routines ----------------------------------------*/
-/*
+
 ISR(TIMER1_OVF_vect)
 {
-	static int i = 0;
-	uint8_t data[8];
+    static int i = 0;
+    uint8_t data[8];
     
-	uint32_t utemp;
-	uint32_t upress;
-	uint32_t uhum;
+    uint32_t utemp;
+    uint32_t upress;
+    uint32_t uhum;
 
-	if (i <= 150)//30=1s
-	{
-		i++;
-	} 
-	else
-	{
-		i = 0;
-		uart_puts("\f");
-		
-		//1--Read TempPressHum
-		result = twi_start((0x76<<1) + TWI_WRITE);
-					
-		//Result
-		itoa(result, uart_string, 10);
-		uart_puts("result:");
-		uart_puts(uart_string);
-		uart_puts("\r\n");
-		
-		if (!result)
-		{
-			twi_write(0xF7);
-			
-			twi_start((0x76<<1) + TWI_READ);
+    if (i <= 150)//30=1s
+    {
+        i++;
+    }
+    else
+    {
+        i = 0;
+        
+        //1--Read TempPressHum
+        result = twi_start((0x76<<1) + TWI_WRITE);
+                    
+        //Result
+        itoa(result, uart_string, 10);
+        uart_puts("data read: ");
+        uart_puts(uart_string);
+        uart_puts("\r\n");
+        
+        if (!result)
+        {
+            twi_write(0xF7);
+            
+            twi_start((0x76<<1) + TWI_READ);
             for(int i = 0; i < 8; i++)
             {
                 data[i] = twi_read_ack();
             }
-		    //data[0] = twi_read_ack(); //press msb
-			//data[1] = twi_read_ack(); //press lsb
-			//data[2] = twi_read_ack(); //press xlsb
-			//data[3] = twi_read_ack(); //temp msb
-			//data[4] = twi_read_ack(); //temp lsb
-			//data[5] = twi_read_ack(); //temp xlsb
-			//data[6] = twi_read_ack(); //hum msb
-			//data[7] = twi_read_ack(); //hum lsb
-			twi_stop();
-		          
+            //data[0] = twi_read_ack(); //press msb
+            //data[1] = twi_read_ack(); //press lsb
+            //data[2] = twi_read_ack(); //press xlsb
+            //data[3] = twi_read_ack(); //temp msb
+            //data[4] = twi_read_ack(); //temp lsb
+            //data[5] = twi_read_ack(); //temp xlsb
+            //data[6] = twi_read_ack(); //hum msb
+            //data[7] = twi_read_ack(); //hum lsb
+            twi_stop();
+                  
             upress = ((uint32_t)data[0] << 12) | ((uint32_t)data[1] << 4) | ((uint32_t)data[2] >> 4);
-			utemp = ((uint32_t)data[3] << 12) | ((uint32_t)data[4] << 4) | ((uint32_t)data[5] >> 4);
-			uhum = ((uint32_t)data[6] << 8) | ((uint32_t)data[7]);
+            utemp = ((uint32_t)data[3] << 12) | ((uint32_t)data[4] << 4) | ((uint32_t)data[5] >> 4);
+            uhum = ((uint32_t)data[6] << 8) | ((uint32_t)data[7]);
 
             press = (int32_t)upress;
             temp = (int32_t)utemp;
             hum = (int32_t)uhum;
-                        
+            /*         
             ltoa(upress, uart_string, 16);
-			uart_puts(uart_string);
-			uart_puts("\r\n");
+            uart_puts(uart_string);
+            uart_puts("\r\n");
             ltoa(utemp, uart_string, 16);
             uart_puts(uart_string);
             uart_puts("\r\n");
             ltoa(uhum, uart_string, 16);
             uart_puts(uart_string);
-            uart_puts("\r\n");
+            uart_puts("\r\n");*/
       
-		}
-	}
-}*/
-/*
+        }
+    }
+}
+
 ISR(TIMER0_OVF_vect)
 {
     static int i = 0;
-    	if (i <= 2000)//250=1s
-    	{
-        	i++;
-    	}
-    	else
-    	{
+        if (i <= 2000)//250=1s
+        {
+            i++;
+        }
+        else
+        {
             i = 0;
+            uart_puts("\f");
+            
             c_temp = comp_temp(temp);
             ltoa(c_temp, uart_string, 10);
-            //uart_puts("uart_string");
-            
-            c_press = comp_press(press);
+            uart_puts("temp: ");
+            uart_puts(uart_string);
+            uart_puts("\r\n");
+            /*
+            c_press = comp_press(press)/press_frac;
+            //press1 = (float)c_press;
+            //press1 = press1/press_frac;
             ltoa(c_press, uart_string, 10);
-            //uart_puts("uart_string");
-            
-            c_hum = comp_hum(hum);
+            uart_puts("press: ");
+            uart_puts(uart_string);
+            uart_puts("\r\n");*/
+           
+            c_hum = comp_hum(hum)/hum_frac;
             ltoa(c_hum, uart_string, 10);
-            //uart_puts("uart_string");
-        }        
-}*/
-/*
+            uart_puts("hum: ");
+            uart_puts(uart_string);
+            uart_puts("\r\n");
+            
+        }
+}
+
 // Returns temperature in DegC, resolution is 0.01 DegC. Output value of “5123” equals 51.23DegC.
 // t_fine carries fine temperature as global value
 int32_t comp_temp(int32_t temp)
@@ -303,14 +310,14 @@ int32_t comp_temp(int32_t temp)
     int32_t var1, var2, T;
     
     var1 = ((((temp>>3) - ((int32_t)dig_T1<<1))) * ((int32_t)dig_T2)) >> 11;
-    var2 = (((((temp>>4) - ((int32_t)dig_T1)) * ((temp>>4) - ((int32_t)dig_T1))) 
-    >> 12) * ((int32_t)dig_T3)) >> 14;
+    var2 = (((((temp>>4) - ((int32_t)dig_T1)) * ((temp>>4) - ((int32_t)dig_T1)))
+           >> 12) * ((int32_t)dig_T3)) >> 14;
     t_fine = var1 + var2;
     T = (t_fine * 5 + 128) >> 8;
     return T;
 }
 
-// Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format 
+// Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format
 //(24 integer bits and 8 fractional bits).
 // Output value of “24674867” represents 24674867/256 = 96386.2 Pa = 963.862 hPa
 uint32_t comp_press(int32_t press)
@@ -325,7 +332,7 @@ uint32_t comp_press(int32_t press)
     var1 = (((((int64_t)1)<<47)+var1))*((int64_t)dig_P1)>>33;
     if (var1 == 0)
     {
-        return 0;
+        return 0;   // avoid exception caused by division by zero
     }
     p = 1048576 - press;
     p = (((p<<31)-var2)*3125)/var1;
@@ -342,16 +349,16 @@ uint32_t comp_hum(int32_t hum)
 {
     int32_t v_x1_u32r;
     
-    v_x1_u32r = (t_fine - ((int32_t)76800));    
+    v_x1_u32r = (t_fine - ((int32_t)76800));
     v_x1_u32r = (((((hum << 14) - (((int32_t)dig_H4) << 20) - (((int32_t)dig_H5)*
-        v_x1_u32r)) + ((int32_t)16384)) >> 15) * (((((((v_x1_u32r * ((int32_t)dig_H6))
-        >>10)*(((v_x1_u32r * ((int32_t)dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + 
-        ((int32_t)2097152)) * ((int32_t)dig_H2)+ 8192) >> 14));        
+        v_x1_u32r)) + ((int32_t)16384)) >> 15) * (((((((v_x1_u32r *
+        ((int32_t)dig_H6))>>10)*(((v_x1_u32r * ((int32_t)dig_H3)) >> 11) + 
+        ((int32_t)32768))) >> 10) + ((int32_t)2097152)) * ((int32_t)dig_H2)+
+        8192) >> 14));
     v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7)*
-        ((int32_t)dig_H1)) >> 4));       
-    v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);    
+        ((int32_t)dig_H1)) >> 4));
+    v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
     v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
     
     return (uint32_t)(v_x1_u32r>>12);
 }
-*/
